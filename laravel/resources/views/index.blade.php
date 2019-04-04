0b32ce8658afb614062371ef459912a4baaefe17
Ben Douglas <dougers437@gmail.com>
2019-04-04 17:51:28 -0400

    Working on backend functionality and implementing laravel

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class = "bg-info text-white"><div class = "container text-center"><div class = "col"><br><br><img src = "https://life-cc.org/wp-content/uploads/icon-members-login.png" height = "200px"><h1>Try Teach-It for Free Today</h1><br><button type="button" class="btn btn-outline-light"><a class = "text-white" href="register.php">Get Started</a></button><br><br><br></div></div></div>
+
+    
+<div class="container d-flex justify-content-center" >
+  <div class="row">
+      
+          <div class="col-md-6 text-left">
+<br><br><br><br>
+      <br><h1>What is Teach-It?</h1><br/><br>
+              
+              <h6>An interactive chat messaging system for teachers and students.</h6><br>
+              
+  <button type="button" class="btn btn-info mr-3"><a class = "text-white" href="about.php">Learn More</a></button>   
+      </div>
+        
+   
+  
+                           
+    <div class="col-md-6 text-right">
+
+              <br><br><br><img src ="https://upload.wikimedia.org/wikipedia/commons/7/72/Message-icon-grey.png" height = "300px" width = "300px"> 
+                </div>
+    </div>
+    </div>
+    <br><br><br><br>
+@endsection
---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Join Course Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,700,800" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
+  <script src="assets/js/main.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+    
+  <nav class="navbar navbar-expand-sm bg-white">
+    
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item"><img  class = "ml-3 mr-3" src="https://openclipart.org/download/289348/Pencil-2d.svg" width="60" alt="Colorful Rainbow Clipart" />
+            </li>
+ <li class="nav-item">
+            <h1>Teach-It</h1>
+            </li>
+    </ul>
+
+                <ul class="navbar-nav ml-auto">
+      <li class = "nav-item"><button type="button" class="btn btn-link mr-3"><a class = "text-muted" href="index.php">Home</a></button></li>
+                  <li class = "nav-item"><button type="button" class="btn btn-link mr-3"><a class = "text-muted" href="about.php">About Us</a></button></li>
+                     <li class = "nav-item"><button type="button" class="btn btn-link mr-3"><a class = "text-muted" href="create-course.php">Create a Chat</a></button></li>
+            <li class = "nav-item"><button type="button" class="btn btn-link mr-3"><a class = "text-muted" href="join-course.php">Join a Chat</a></button></li>
+       
+            <li class = "nav-item"><button type="button" class="btn btn-info mr-3"><a class = "text-white" href="login.php">Login</a></button></li>
+            <li class = "nav-item"><button type="button" class="btn btn-info mr-3"><a class = "text-white" href="register.php">Register</a></button></li>
+   
+
+    </ul>
+    
+    </nav>  
+    
+    @yield('content')
+
+  <nav class="navbar navbar-expand-lg bg-white">
+        <ul class="navbar-nav mr-auto">
+     
+ 
+    </ul>
+
+                <ul class="navbar-nav ml-auto">
+                    <li class = "nav-item">
+     © 2019 <img src="https://openclipart.org/download/289348/Pencil-2d.svg" width="20" alt="Colorful Rainbow Clipart" /> Teach-It, Inc. All rights reserved
+   
+                    </li>
+    </ul>
+    
+    </nav> 
+
+</body>
+
+</html>
